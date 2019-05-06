27ff3d0b8c7603143ea010409f8a5456a1314972
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-05-05 20:50:13 -0400

    index.js mouseover event.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 // Your code goes here
 
 //mouseover event listener
+
+let navMouseover = document.querySelector(".nav");
+navMouseover.addEventListener('mouseover', (event)=>{
+    event.target.style.backgroundColor = 'transparent';
+    event.target.style.color = 'brown';
+});
