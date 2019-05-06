9e24a0098b8674b87b9a405c874fb7c20e42afa3
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-05-05 21:37:52 -0400

    index.js accessibility zoom on mousewheel event.

--- js/index.js
+++ js/index.js
@@ -8,9 +8,33 @@ navMouseover.addEventListener('mouseover', (event)=>{
     event.target.style.color = 'brown';
 });
 
-//
-
+//log keyboard events
 
 document.addEventListener('keydown', (event)=>{
     console.log(event);
 });
+
+// Zoom on Mousewheel Event.  Copied from Mozilla developer network
+function zoom(event) {
+    event.preventDefault();
+  
+    if (event.deltaY < 0) {
+      // Zoom in
+      scale *= event.deltaY * -2;
+    }
+    else {
+      // Zoom out
+      scale /= event.deltaY * 2;
+    }
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('div');
+  el.onwheel = zoom;
+  
